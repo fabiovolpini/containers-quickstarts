c431dec420f5fa3013e238938d3287eedc23237a
fabiovolpini <fabiovolpini@gmail.com>
2019-08-16 14:24:52 -0300

    Create get-prometheus-plugin.sh

---
+++ rabbitmq/get-prometheus-plugin.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# make sure the directory exists
+mkdir -p /opt/rabbitmq/plugins
+cd /opt/rabbitmq/plugins
+
+# Downloads prometheus_rabbitmq_exporter and its dependencies with curl
+
+readonly base_url='https://github.com/deadtrickster/prometheus_rabbitmq_exporter/releases/download/v3.7.9.1'
+
+get() {
+  curl -LO "$base_url/$1"
+}
+
+get accept-0.3.5.ez
+get prometheus-4.3.0.ez
+get prometheus_cowboy-0.1.7.ez
+get prometheus_httpd-2.1.10.ez
+get prometheus_rabbitmq_exporter-3.7.9.1.ez
